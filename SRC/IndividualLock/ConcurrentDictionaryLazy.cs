15615667eaf41d135b5a83eb4cdec32197757093
Oscar <csknife@163.com>
2019-03-31 22:47:18 +0800

    create

---
+++ SRC/IndividualLock/ConcurrentDictionaryLazy.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IndividualLock
+{
+    class ConcurrentDictionaryLazy<TKey, TValue> : IDictionary<TKey, TValue>
+    {
+        readonly ConcurrentDictionary<TKey, Lazy<TValue>> dictionary;
+
+        public ConcurrentDictionaryLazy(IEqualityComparer<TKey> comparer = null)
+        {
+            if (comparer == null)
+                comparer = EqualityComparer<TKey>.Default;
+
+            this.dictionary = new ConcurrentDictionary<TKey, Lazy<TValue>>(comparer);
+        }
+
+        static Lazy<TValue> NewValue(TValue value)
+        {
+            return new Lazy<TValue>(() => value);
+        }
+
+        static Lazy<TValue> NewValue(TKey key, Func<TKey, TValue> addValueFactory)
+        {
+            return new Lazy<TValue>(() => addValueFactory(key));
+        }
+
+        static Lazy<TValue> NewValue(TKey key, Lazy<TValue> value, Func<TKey, TValue, TValue> updateValueFactory)
+        {
+            return new Lazy<TValue>(() => updateValueFactory(key, value.Value));
+        }
+
+        public bool TryAdd(TKey key, Func<TKey, TValue> valueFactory)
+        {
+            return this.dictionary.TryAdd(key, NewValue(key, valueFactory));
+        }
+
+        public TValue GetOrAdd(TKey key, Func<TKey, TValue> valueFactory)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+
+            if (valueFactory == null)
+                throw new ArgumentNullException(nameof(valueFactory));
+
+            return this.dictionary.GetOrAdd(key, k => NewValue(k, valueFactory)).Value;
+        }
+
+        /// <summary>
+        /// Update will update existing value
+        /// </summary>
+        public TValue AddOrUpdate(TKey key, Func<TKey, TValue> addValueFactory, Action<TKey, TValue> updateValueFactory)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+
+            if (addValueFactory == null)
+                throw new ArgumentNullException(nameof(addValueFactory));
+
+            if (updateValueFactory == null)
+                throw new ArgumentNullException(nameof(updateValueFactory));
+
+            return this.dictionary.AddOrUpdate(key, k => NewValue(k, addValueFactory), (k, v) =>
+            {
+                updateValueFactory(k, v.Value);
+                return v;
+            }).Value;
+        }
+
+        /// <summary>
+        /// Update will replace existing value
+        /// </summary>
+        public TValue AddOrUpdate(TKey key, Func<TKey, TValue> addValueFactory, Func<TKey, TValue, TValue> updateValueFactory)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+
+            if (addValueFactory == null)
+                throw new ArgumentNullException(nameof(addValueFactory));
+
+            if (updateValueFactory == null)
+                throw new ArgumentNullException(nameof(updateValueFactory));
+
+            return this.dictionary.AddOrUpdate(key, k => NewValue(k, addValueFactory), (k, v) => NewValue(k, v, updateValueFactory)).Value;
+        }
+
+        #region IEnumerable
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            foreach (var kv in this.dictionary)
+            {
+                yield return new KeyValuePair<TKey, TValue>(kv.Key, kv.Value.Value);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+        static TValue CheckValue(Lazy<TValue> value)
+        {
+            return value == null ? default(TValue) : value.Value;
+        }
+
+        public bool TryRemove(TKey key, out TValue value)
+        {
+            var result = this.dictionary.TryRemove(key, out var lazy);
+            value = CheckValue(lazy);
+            return result;
+        }
+
+        #region IDictionary
+
+        public bool ContainsKey(TKey key)
+        {
+            return this.dictionary.ContainsKey(key);
+        }
+
+        void IDictionary<TKey, TValue>.Add(TKey key, TValue value)
+        {
+            this.dictionary.TryAdd(key, NewValue(value));
+        }
+
+        public bool Remove(TKey key)
+        {
+            return this.dictionary.TryRemove(key, out _);
+        }
+
+        void ICollection<KeyValuePair<TKey, TValue>>.Add(KeyValuePair<TKey, TValue> item)
+        {
+            ((IDictionary<TKey, TValue>)this).Add(item.Key, item.Value);
+        }
+
+        public void Clear()
+        {
+            this.dictionary.Clear();
+        }
+
+        bool ICollection<KeyValuePair<TKey, TValue>>.Contains(KeyValuePair<TKey, TValue> item)
+        {
+            return TryGetValue(item.Key, out var value) && EqualityComparer<TValue>.Default.Equals(value, item.Value);
+        }
+
+        void ICollection<KeyValuePair<TKey, TValue>>.CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            var items = new KeyValuePair<TKey, Lazy<TValue>>[array.Length];
+            ((ICollection<KeyValuePair<TKey, Lazy<TValue>>>)this.dictionary).CopyTo(items, arrayIndex);
+            for (var i = arrayIndex; i < items.Length; i++)
+            {
+                var current = items[i];
+                array[i] = new KeyValuePair<TKey, TValue>(current.Key, current.Value.Value);
+            }
+        }
+
+        bool ICollection<KeyValuePair<TKey, TValue>>.Remove(KeyValuePair<TKey, TValue> item)
+        {
+            return Remove(item.Key);
+        }
+
+        public int Count => this.dictionary.Count;
+
+        bool ICollection<KeyValuePair<TKey, TValue>>.IsReadOnly => ((ICollection<KeyValuePair<TKey, Lazy<TValue>>>)this.dictionary).IsReadOnly;
+
+        public ICollection<TKey> Keys => this.dictionary.Keys;
+
+        public ICollection<TValue> Values
+        {
+            get { return this.dictionary.Values.Select(s => s.Value).ToList().AsReadOnly(); }
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            var result = this.dictionary.TryGetValue(key, out var lazy);
+            value = CheckValue(lazy);
+            return result;
+        }
+
+        public TValue this[TKey key]
+        {
+            get => CheckValue(this.dictionary[key]);
+            set => this.dictionary[key] = NewValue(value);
+        }
+
+        #endregion
+    }
+}
---
+++ SRC/IndividualLock/IndividualLock.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Nito.AsyncEx;
+
+namespace IndividualLock
+{
+    public abstract class IndividualLock<TKey, TValue>
+        where TValue : new()
+    {
+        class LockingObject
+        {
+            internal LockingObject(DateTime? expiry = null)
+            {
+                Expiry = expiry;
+            }
+
+            internal readonly TValue Data = new TValue();
+            internal DateTime? Expiry { get; set; }
+        }
+
+        readonly TimeSpan? expiration;
+        readonly ConcurrentDictionaryLazy<TKey, LockingObject> objects;
+        DateTime nextCheckTime;
+
+        protected IndividualLock(IEqualityComparer<TKey> comparer = null, TimeSpan? expiration = null)
+        {
+            this.expiration = expiration;
+            this.nextCheckTime = expiration == null ? DateTime.MaxValue : GetNextCheckTime(DateTime.Now);
+            this.objects = new ConcurrentDictionaryLazy<TKey, LockingObject>(comparer ?? EqualityComparer<TKey>.Default);
+        }
+
+        protected TValue GetLock(TKey key)
+        {
+            var now = DateTime.Now;
+
+            RemoveExpiries(now);
+
+            var expiry = this.expiration == null ? (DateTime?)null : GetNextCheckTime(now);
+            return this.objects.AddOrUpdate(key, k => new LockingObject(expiry), (k, v) => v.Expiry = expiry).Data;
+        }
+
+        DateTime GetNextCheckTime(DateTime now)
+        {
+            return now.AddMilliseconds(this.expiration.Value.TotalMilliseconds);
+        }
+
+        bool RequireRemoveExpiries(DateTime now)
+        {
+            return this.expiration != null && this.nextCheckTime <= now;
+        }
+
+        void RemoveExpiries(DateTime now)
+        {
+            if (!RequireRemoveExpiries(now))
+                return;
+
+            lock (this.objects)
+            {
+                if (!RequireRemoveExpiries(now))
+                    return;
+
+                var nextTime = GetNextCheckTime(now);
+
+                var expiries = this.objects.Where(w =>
+                {
+                    var expiry = w.Value.Expiry.Value;
+
+                    if (expiry <= now)
+                        return true;
+
+                    if (expiry < nextTime)
+                        nextTime = expiry;
+
+                    return false;
+                }).ToList();
+
+                expiries.ParallelForEach(kv =>
+                {
+                    var obj = kv.Value.Data;
+                    var asyncLock = obj as AsyncLock;
+
+                    if (asyncLock == null && !obj.IsLocked()
+                        || asyncLock != null && !asyncLock.IsLocked())
+                        this.objects.Remove(kv.Key);
+                });
+
+                this.nextCheckTime = nextTime;
+            }
+        }
+    }
+
+    #region string / object
+
+    public class IndividualLock<TKey> : IndividualLock<TKey, object>
+    {
+        public IndividualLock(IEqualityComparer<TKey> comparer) : this(comparer, null) { }
+
+        public IndividualLock(TimeSpan? expiration) : this(null, expiration) { }
+
+        public IndividualLock(IEqualityComparer<TKey> comparer = null, TimeSpan? expiration = null) : base(comparer, expiration) { }
+
+        public object Lock(TKey key)
+        {
+            return GetLock(key);
+        }
+    }
+
+    public class IndividualLock : IndividualLock<string>
+    {
+        public IndividualLock(IEqualityComparer<string> comparer) : this(comparer, null) { }
+
+        public IndividualLock(TimeSpan? expiration) : this(null, expiration) { }
+
+        public IndividualLock(IEqualityComparer<string> comparer = null, TimeSpan? expiration = null) : base(comparer ?? StringComparer.Ordinal, expiration) { }
+    }
+
+    #endregion
+
+    #region string / AsyncLock
+
+    public class IndividualLockAsync<TKey> : IndividualLock<TKey, AsyncLock>
+    {
+        public IndividualLockAsync(IEqualityComparer<TKey> comparer) : this(comparer, null) { }
+
+        public IndividualLockAsync(TimeSpan? expiration) : this(null, expiration) { }
+
+        public IndividualLockAsync(IEqualityComparer<TKey> comparer = null, TimeSpan? expiration = null) : base(comparer, expiration) { }
+
+        public IDisposable Lock(TKey key, CancellationToken? cancellationToken = null)
+        {
+            return GetLock(key).Lock(cancellationToken ?? CancellationToken.None);
+        }
+
+        public AwaitableDisposable<IDisposable> LockAsync(TKey key, CancellationToken? cancellationToken = null)
+        {
+            return GetLock(key).LockAsync(cancellationToken ?? CancellationToken.None);
+        }
+    }
+
+    public class IndividualLockAsync : IndividualLockAsync<string>
+    {
+        public IndividualLockAsync(IEqualityComparer<string> comparer) : this(comparer, null) { }
+
+        public IndividualLockAsync(TimeSpan? expiration) : this(null, expiration) { }
+
+        public IndividualLockAsync(IEqualityComparer<string> comparer = null, TimeSpan? expiration = null) : base(comparer ?? StringComparer.Ordinal, expiration) { }
+    }
+
+    #endregion
+}
